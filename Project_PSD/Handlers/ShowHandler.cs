bd7ef19637ae899c6947065370849af2fe7ae056
RAYMOND CHRISANDY RUNTUWENE <raymond.runtuwene@binus.ac.id>
2021-05-16 22:38:01 +0800

    ShowHandlerTest

---
+++ Project_PSD/Handlers/ShowHandler.cs
@@ -0,0 +1,17 @@
+﻿using Project_PSD.Models;
+using Project_PSD.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_PSD.Handler
+{
+    public class ShowHandler
+    {
+        public static List<Show> GetShowList()
+        {
+            return ShowRepository.GetAllShows();
+        }
+    }
+}
